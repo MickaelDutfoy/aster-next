ffd295de7882334893cd32900e2ddb703b631b9e
MickaelDutfoy <m.dutfoy@gmail.com>
2025-10-25 14:08:47 +0200

    disabled SW

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,2 @@
-const CACHE = 'aster-v1';
-const ASSETS = ['/', '/manifest.webmanifest'];
-
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
-self.addEventListener('fetch', (e) => {
-  const { request } = e;
-  if (request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(request).then(
-      (cached) =>
-        cached ||
-        fetch(request)
-          .then((res) => {
-            const copy = res.clone();
-            caches.open(CACHE).then((c) => c.put(request, copy));
-            return res;
-          })
-          .catch(() => cached),
-    ),
-  );
-});
