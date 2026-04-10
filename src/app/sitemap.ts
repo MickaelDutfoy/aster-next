85fe6355564e95114d24dec06d7ca67780afc513
MickaelDutfoy <m.dutfoy@gmail.com>
2026-04-10 18:55:46 +0200

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,7 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
   return locales.map((locale) => ({
     url: `${baseUrl}/${locale}/discover`,
-    lastModified: new Date(),
+    lastModified: new Date().toISOString().split('T')[0],
     changeFrequency: 'monthly',
     priority: 1.0,
   }));
