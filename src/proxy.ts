31cfbcc54478afc4d343133421582d56ebf2541f
MickaelDutfoy <m.dutfoy@gmail.com>
2026-04-10 17:45:47 +0200

    matcher middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -151,5 +151,5 @@ export const proxy = authMw(handler, {
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
 };
