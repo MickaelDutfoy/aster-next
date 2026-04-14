b92966de5e41385a981d8801f5b86131983bbd4b
MickaelDutfoy <m.dutfoy@gmail.com>
2026-04-14 16:32:53 +0200

    sitemap unrefix

--- src/proxy.ts
+++ src/proxy.ts
@@ -148,5 +148,7 @@ export const proxy = authMw(handler, {
 });
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|sitemap-static.xml).*)',
+  ],
 };
